17592d22171544f6bd4bed0adef80d5220101406
fbu <florensvb@gmail.com>
2019-06-15 13:36:49 +0200

    upload paper and ipfs

--- researchain/researchain-front/src/components/Shop.vue
+++ researchain/researchain-front/src/components/Shop.vue
@@ -3,16 +3,29 @@
     <v-layout
       text-xs-center
       wrap
+      row
     >
-      <v-flex mb-4>
+      <v-flex xs12>
         <h1 class="display-2 font-weight-bold mb-3">
           Researchain Shop
         </h1>
       </v-flex>
-      <v-flex xs12>
-        <v-btn color="teal accent-4" @click="$router.push('/shop')">
+      <v-flex>
+        <v-text-field
+          label="Title"
+          single-line
+          box
+          v-model="title"
+        ></v-text-field>
+      </v-flex>
+      <v-flex>
+        <v-btn color="teal accent-4" @click="createPaper">
           <span class="mr-2">Upload Paper</span>
         </v-btn>
+      </v-flex>
+      <v-flex xs12>
+        <v-btn @click="$refs.inputUpload.click()">Upload File</v-btn>
+        <input v-show="false" ref="inputUpload" type="file" accept="application/pdf" @change="saveToIpfsWithFilename" >
       </v-flex>
     </v-layout>
   </v-container>
@@ -21,7 +34,38 @@
 <script>
   export default {
     data: () => ({
-
-    })
+      title: '',
+    }),
+    methods: {
+      createPaper() {
+        try {
+          this.papers.methods._createPaper(this.title).send()
+            .then(console.log("creating paper done"));
+        } catch (e) {
+          console.error(e);
+        }
+      },
+      async saveToIpfsWithFilename ({ target: { files }}) {
+        try {
+          const file = [...files][0];
+          const fileDetails = {
+            path: file.name,
+            content: file
+          };
+          const options = {
+            wrapWithDirectory: true,
+            progress: (prog) => console.log(`received: ${prog}`)
+          };
+          const response = await this.ipfs.add(fileDetails, options);
+          const fileHash = response[response.length - 1].hash;
+          console.log(`File hash: ${fileHash}`);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    },
+    created() {
+      // this.ipfs.g
+    }
   }
 </script>
--- researchain/researchain-front/src/main.js
+++ researchain/researchain-front/src/main.js
@@ -5,12 +5,20 @@ import './plugins/vuetify'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import Web3 from 'web3';
+import * as ipfsClient from 'ipfs-http-client';
 
+// connect to ipfs daemon API server
+const ipfs = ipfsClient('localhost', '5001', { protocol: 'http' }); // leaving out the arguments will default to these values
+Vue.prototype.ipfs = ipfs;
+
+// connect to blockchain via web3
 const web3 = new Web3('ws://localhost:7545');
+web3.defaultAccount = process.env.VUE_APP_ACCOUNT;
+web3.defaultGas = 900000;
 const abi = require('./ABIs/Papers.js').default;
 const papersContract = new web3.eth.Contract(abi, process.env.VUE_APP_ADDRESS);
 papersContract.options.address = process.env.VUE_APP_ADDRESS;
-papersContract.methods.getPaper(0).call().then(console.log);
+Vue.prototype.papers = papersContract;
 
 Vue.use(VueRouter);
 
