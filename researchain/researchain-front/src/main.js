9a11b622fa46b26fc4fa64da5c78c99465ba70db
fbu <florensvb@gmail.com>
2019-06-20 09:08:46 +0200

    ipfs support

--- researchain/researchain-front/src/main.js
+++ researchain/researchain-front/src/main.js
@@ -8,7 +8,7 @@ import Web3 from 'web3';
 import * as ipfsClient from 'ipfs-http-client';
 
 // connect to ipfs daemon API server
-const ipfs = ipfsClient('localhost', '5001', { protocol: 'http' }); // leaving out the arguments will default to these values
+const ipfs = ipfsClient(process.env.VUE_APP_IPFS_ADDRESS, '5001', { protocol: 'http' }); // leaving out the arguments will default to these values
 Vue.prototype.ipfs = ipfs;
 
 // connect to blockchain via web3
