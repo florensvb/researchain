a30c66952988e390b98e52e878001c88f5a50ad3
Alex <a.ziebell@web.de>
2019-06-26 12:58:43 +0200

    conflicts resolved

--- researchain/researchain-back/contracts/Papers.sol
+++ researchain/researchain-back/contracts/Papers.sol
@@ -1,5 +1,5 @@
 // new version from Papers.sol
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.5.0;
 
 import "./Ownable.sol";
 
@@ -7,7 +7,7 @@ import "./Ownable.sol";
 contract Papers is Ownable {
 
     event NewPaper(uint paperId, string title, string hash);
-    event PaperBought(uint paperId, string title, string hash);
+    event PaperBought(address buyer, uint paperId, string title, string hash);
 
     struct Paper {
         string title;
@@ -19,16 +19,22 @@ contract Papers is Ownable {
 
     Paper[] private papers;
 
-    mapping (uint => address) public paperIdToOwner;
-    mapping (address => uint) ownerPaperCount;
-    mapping (address => uint[]) public ownersPaperIds;
+    uint numberOfPapers = 0;
+    mapping (uint => address) public paperIdToOwner; // shows the owner of an paperId
+    mapping (address => uint) ownerPaperCount; // number of papers an owner has
+    mapping (address => uint[]) public ownersPaperIds; // all PaperId's of an owner address
+    mapping (address => bool) public ownerExist; // shows that owner has uploaded a paper
+    mapping (address => bool) public buyerExist; // shows that buyer already bought something
+    mapping (address => uint[]) public buyersPaperIds; // all PaperId's bought by a buyer address
 
     // createPaper gets called by JavaScript with values
-    function _createPaper(string memory _title, string memory _author, uint _price, string memory _hash) public {
+    function createPaper(string memory _title, string memory _author, uint _price, string memory _hash) public {
         uint id = papers.push(Paper(_title, _author, _price, _hash, msg.sender)) - 1;
         paperIdToOwner[id] = msg.sender;
         ownerPaperCount[msg.sender]++;
         ownersPaperIds[msg.sender].push(uint(id)); // working?
+        ownerExist[msg.sender] = true;
+        numberOfPapers++;
         emit NewPaper(id, _title, _hash);
     }
 
@@ -41,22 +47,36 @@ contract Papers is Ownable {
         return (_id, papers[_id].title, papers[_id].author, papers[_id].price, papers[_id].hash, papers[_id].owner);
     }
 
+    function getOwnersPapers(address _owner) public view returns (uint[] memory) {
+        require(ownerExist[_owner], "Owner has no Papers!");
+        return (ownersPaperIds[_owner]);
+    }
+
+    function getBuyersPapers(address _owner) public view returns (uint[] memory) {
+        require(buyerExist[_owner], "Buyer bought no Papers!");
+        return (buyersPaperIds[_owner]);
+    }
+
     function buyPaper(uint _id) external payable {
-      require(msg.value >= papers[_id].price);
+      require(numberOfPapers > _id, "paper id does not exist!");
+      require(msg.value >= papers[_id].price, "not sufficient funds added to buyPaper function call!");
 
       // caller sends value to contract according papers[id].price
       bool transferConfirmed = msg.sender.send(papers[_id].price);
 
       // transfer papers[id].hash to caller, if paid
-      if (transferConfirmed == true) emit PaperBought(_id, papers[_id].title, papers[_id].hash);
+      if (transferConfirmed == true) emit PaperBought(msg.sender, _id, papers[_id].title, papers[_id].hash);
 
-      withdraw(_id); // withdraw value to owner -> see function withdraw ?
+      buyerExist[msg.sender] = true;
+      buyersPaperIds[msg.sender].push(uint(_id)); // push bought paperId on buyers account
+
+      _withdraw(_id); // withdraw value to owner -> see function withdraw ?
     }
 
     // PaperStore needs to point to contract on Ethereum
     // JS Code:
     // PaperStore.buyPaper({from: web3.eth.defaultAccount, value: web3.utils.toWei(0.001)})
-    function withdraw(uint _id) public onlyOwner {
+    function _withdraw(uint _id) internal onlyOwner {
       address payable owner = papers[_id].owner;
       owner.transfer(address(this).balance);
     }
