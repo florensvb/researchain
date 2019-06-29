1820b0ad1214d6bbeb0fbe6948c812a7d4b3068c
fbu <florensvb@gmail.com>
2019-06-29 15:08:03 +0200

    fix bug after connecting to metamask

--- researchain/researchain-front/src/main.js
+++ researchain/researchain-front/src/main.js
@@ -11,29 +11,33 @@ import * as ipfsClient from 'ipfs-http-client';
 const ipfs = ipfsClient(process.env.VUE_APP_IPFS_ADDRESS, '5001', { protocol: 'http' }); // leaving out the arguments will default to these values
 Vue.prototype.ipfs = ipfs;
 
-if (window.ethereum) {
-  try {
-    // Request account access
-    const web3 = new Web3('ws://localhost:7545');
-    window.ethereum.enable();
-    web3.defaultAccount = window.ethereum.selectedAddress;
-    web3.defaultAccount = window.ethereum.selectedAddress;
-    web3.defaultGas = 900000;
-    const abi = require('./ABIs/Papers.js').default;
-    const papersContract = new web3.eth.Contract(abi, process.env.VUE_APP_ADDRESS);
-    papersContract.options.address = process.env.VUE_APP_ADDRESS;
-    Vue.prototype.paperContract = papersContract;
-    Vue.prototype.web3 = web3;
+const initweb3 = async () => {
+  if (window.ethereum) {
+    try {
+      // Request account access
+      const web3 = new Web3('ws://localhost:7545');
+      const accounts = await window.ethereum.enable();
+      web3.defaultAccount = accounts[0];
+      web3.defaultAccount = accounts[0];
+      web3.defaultGas = 900000;
+      const abi = require('./ABIs/Papers.js').default;
+      const papersContract = new web3.eth.Contract(abi, process.env.VUE_APP_ADDRESS);
+      papersContract.options.address = process.env.VUE_APP_ADDRESS;
+      Vue.prototype.paperContract = papersContract;
+      Vue.prototype.web3 = web3;
 
-    window.ethereum.on('accountsChanged', function () {
-      window.location.reload();
-    })
-  } catch (error) {
-    // User denied account access...
-    console.error(error);
-    console.error("User denied account access")
+      window.ethereum.on('accountsChanged', function () {
+        window.location.reload();
+      })
+    } catch (error) {
+      // User denied account access...
+      console.error(error);
+      console.error("User denied account access")
+    }
   }
-}
+};
+
+initweb3().catch(console.error);
 
 Vue.use(VueRouter);
 
